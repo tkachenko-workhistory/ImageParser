50cf52ca0d010d04fe01b5d4c50085654bb13513
Иван <@DESKTOP-HTQVO4V>
2019-09-30 22:57:47 +0500

    Bmp исправлена ошибка в оперделении ширины и высоты, и поставлена заглушка

--- ImageParser.cs
+++ ImageParser.cs
@@ -22,7 +22,9 @@ namespace ImageParser
             else if (s.Contains("gif"))
                 info = GetGifInfo(stream);
             else if (buffer[0] == 0x42 && buffer[1] == 0x4D)
-                info = GetBmpInfo(stream);
+                info = GetBmpInfo(stream, true);
+            else if (buffer[1] == 0x42 && buffer[0] == 0x4D)
+                info = GetBmpInfo(stream, true);
             else
                 throw new Exception("Неподдерживаемый формат");
 
@@ -32,7 +34,7 @@ namespace ImageParser
         private ImageInfo GetPngInfo(Stream stream)
         {
             var info = new ImageInfo();
-            info.Extension = "png";
+            info.Format = "png";
 
             stream.Position = 16; // Переходим на позицию чтения Chunks
             byte[] buffer = new byte[4];
@@ -51,7 +53,7 @@ namespace ImageParser
         private ImageInfo GetGifInfo(Stream stream)
         {
             var info = new ImageInfo();
-            info.Extension = "gif";
+            info.Format = "gif";
 
             // Ищем блок изображения
             byte[] buffer = new byte[1];
@@ -60,7 +62,6 @@ namespace ImageParser
             {
                 stream.Read(buffer, 0, 1);
                 StringBuilder sb = new StringBuilder(buffer.Length * 2);
-                // ToDo: можно пропускать расширения, узнав их длину в байтах
                 sb.AppendFormat("{0:x2}", buffer[0]);
                 type = sb.ToString();
             }
@@ -79,41 +80,46 @@ namespace ImageParser
             return info;
         }
 
-        private ImageInfo GetBmpInfo(Stream stream)
+        private ImageInfo GetBmpInfo(Stream stream, bool bigEndian)
         {
             var info = new ImageInfo();
-            info.Extension = "bmp";
+            info.Format = "bmp";
 
+            // Определить версию структуры
             byte[] buffer = new byte[4];
+            stream.Position = 14;
             stream.Read(buffer, 0, 4);
-            info.Size = BitConverter.ToInt32(buffer, 0);
+            int version = BitConverter.ToInt32(buffer, 0);
 
-            buffer = new byte[7];
-            stream.Position = 16;
-            stream.Read(buffer, 0, 4);
-            for (int i = 0; i < 4; i++)
-                if (buffer[i] != 0)
-                {
-                    info.Width = BitConverter.ToInt32(buffer, i);
-                    break;
-                }
+            info.Size = stream.Length;
 
-            stream.Position = 20;
-            stream.Read(buffer, 0, 4);
-            for (int i = 0; i < 4; i++)
-                if (buffer[i] != 0)
-                {
-                    info.Height = BitConverter.ToInt32(buffer, i);
-                    break;
-                }
+            if (!bigEndian)
+                throw new Exception("littleEndian " + info.Height);
 
+            if (version == 40 || version == 108 || version == 124)
+            // Соответствует 3, 4 и 5 версии
+            {
+                buffer = new byte[4];
+                stream.Position = 18;
+                stream.Read(buffer, 0, 4);
+                info.Width = BitConverter.ToInt32(buffer, 0);
+
+                buffer = new byte[4];
+                stream.Read(buffer, 0, 4);
+                info.Height = BitConverter.ToInt32(buffer, 0);
+            }
+            else if (version == 12)
+            // CORE
+            {
+                throw new Exception("core " + info.Height);
+            }
             return info;
         }
 
 
         class ImageInfo
         {
-            public string Extension { get; set; }
+            public string Format { get; set; }
 
             public int Width { get; set; }
 
