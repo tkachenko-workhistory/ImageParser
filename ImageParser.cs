d42706c48964f9677fb620232f61b613c0dba048
Иван <@DESKTOP-HTQVO4V>
2019-09-30 21:30:15 +0500

    Методы

--- ImageParser.cs
+++ ImageParser.cs
@@ -9,66 +9,51 @@ namespace ImageParser
     {
         public string GetImageInfo(Stream stream)
         {
-            // PNG
-            /* byte[] buffer = new byte[3];
-             stream.Position = 1; // Первый байт пропускаем ибо там non-ASCII
+            byte[] buffer = new byte[4];
+            // Определяем тип файла
+            stream.Read(buffer, 0, 4);
+            string s = Encoding.UTF8.GetString(buffer, 0, buffer.Length).ToLower();
 
-             // Читаем расширение
-             stream.Read(buffer, 0, 3); 
-             string s = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+            ImageInfo info;
 
-             stream.Position = 16; // Переходим на позицию чтения Chunks
-             buffer = new byte[4];
-             stream.Read(buffer, 0, 4);
-             var width = BitConverter.ToInt32(new byte[4] { buffer[3], buffer[2], buffer[1], buffer[0] }, 0);
+            if (s.Contains("png"))
+                info = GetPngInfo(stream);
+            else if (s.Contains("gif"))
+                info = GetGifInfo(stream);
+            else if (buffer[0] == 0x42 && buffer[1] == 0x4D)
+                info = GetBmpInfo(stream);
+            else
+                throw new Exception("Неподдерживаемый формат");
 
-             buffer = new byte[4];
-             stream.Read(buffer, 0, 4);
-             var height = BitConverter.ToInt32(new byte[4] { buffer[3], buffer[2], buffer[1], buffer[0] }, 0);
+            return "";
+        }
 
-             long length = stream.Length;*/
+        private ImageInfo GetPngInfo(Stream stream)
+        {
+            var info = new ImageInfo();
+            info.Extension = "png";
 
-            // BMP
-            /* byte[] buffer = new byte[2];
-             stream.Read(buffer, 0, 2);
-             // 4D42/424D (little-endian / big-endian).
-             StringBuilder sb = new StringBuilder(buffer.Length * 2);
-             foreach (byte b in buffer)
-                 sb.AppendFormat("{0:x2}", b);
-             var type = sb.ToString();
+            stream.Position = 16; // Переходим на позицию чтения Chunks
+            byte[] buffer = new byte[4];
+            stream.Read(buffer, 0, 4);
+            info.Width = BitConverter.ToInt32(new byte[4] { buffer[3], buffer[2], buffer[1], buffer[0] }, 0);
 
-             buffer = new byte[4];
-             stream.Read(buffer, 0, 4);
-             var length = BitConverter.ToInt32(buffer, 0);
+            buffer = new byte[4];
+            stream.Read(buffer, 0, 4);
+            info.Height = BitConverter.ToInt32(new byte[4] { buffer[3], buffer[2], buffer[1], buffer[0] }, 0);
 
-             buffer = new byte[7];
-             stream.Position = 16;
-             stream.Read(buffer, 0, 4);
-             var width = 0;
-             for (int i = 0; i < 4; i++)
-                 if (buffer[i] != 0)
-                 {
-                     width = BitConverter.ToInt32(buffer, i);
-                     break;
-                 }
+            info.Size = stream.Length;
 
-             stream.Position = 20;
-             stream.Read(buffer, 0, 4);
-             var height = 0;
-             for (int i = 0; i < 4; i++)
-                 if (buffer[i] != 0)
-                 {
-                     height = BitConverter.ToInt32(buffer, i);
-                     break;
-                 }*/
+            return info;
+        }
 
-            // GIF
-            byte[] buffer = new byte[6];
-            // Читаем расширение
-            stream.Read(buffer, 0, 6);
-            string s = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
-            // ищем блок изображения
-            buffer = new byte[1];
+        private ImageInfo GetGifInfo(Stream stream)
+        {
+            var info = new ImageInfo();
+            info.Extension = "gif";
+
+            // Ищем блок изображения
+            byte[] buffer = new byte[1];
             string type = "";
             while (type != "2c")
             {
@@ -78,18 +63,62 @@ namespace ImageParser
                 sb.AppendFormat("{0:x2}", buffer[0]);
                 type = sb.ToString();
             }
+
             buffer = new byte[4];
             stream.Position += 4;
+
             stream.Read(buffer, 0, 2);
-            var width = BitConverter.ToInt32(buffer, 0);
+            info.Width = BitConverter.ToInt32(buffer, 0);
+
             stream.Read(buffer, 0, 2);
-            var height = BitConverter.ToInt32(buffer, 0);
+            info.Height = BitConverter.ToInt32(buffer, 0);
 
-            long length = stream.Length;
+            info.Size = stream.Length;
+
+            return info;
+        }
+
+        private ImageInfo GetBmpInfo(Stream stream)
+        {
+            var info = new ImageInfo();
+            info.Extension = "bmp";
+
+            byte[] buffer = new byte[4];
+            stream.Read(buffer, 0, 4);
+            info.Size = BitConverter.ToInt32(buffer, 0);
+
+            buffer = new byte[7];
+            stream.Position = 16;
+            stream.Read(buffer, 0, 4);
+            for (int i = 0; i < 4; i++)
+                if (buffer[i] != 0)
+                {
+                    info.Width = BitConverter.ToInt32(buffer, i);
+                    break;
+                }
+
+            stream.Position = 20;
+            stream.Read(buffer, 0, 4);
+            for (int i = 0; i < 4; i++)
+                if (buffer[i] != 0)
+                {
+                    info.Height = BitConverter.ToInt32(buffer, i);
+                    break;
+                }
+
+            return info;
+        }
 
 
-            return "";
+        class ImageInfo
+        {
+            public string Extension { get; set; }
 
+            public int Width { get; set; }
+
+            public int Height { get; set; }
+
+            public long Size { get; set; }
         }
     }
 }
